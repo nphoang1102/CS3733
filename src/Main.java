b2a52285c58098d4145fcd471dd04089e4619b64
Daniel Yun <dyun@wpi.edu>
2017-04-01 16:58:06 -0400

    Test push

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello World!");
         System.out.println("New changes");
-        System.out.println("Test");
 	// write your code here
     }
 }
