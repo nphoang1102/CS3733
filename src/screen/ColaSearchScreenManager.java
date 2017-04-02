8661ec0ca2f3170687d9ccd324c032875e226e10
Hoang Nguyen <nphoang1102@gmail.com>
2017-04-02 14:39:59 -0400

    changed some UI colors and fixed some errors due to copy and paste

--- src/screen/ColaSearchScreenManager.java
+++ src/screen/ColaSearchScreenManager.java
@@ -1,7 +1,32 @@
 package screen;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.*;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Text;
+
+import java.awt.*;
+
 /**
  * Created by Hoang Nguyen on 4/2/2017.
  */
 public class ColaSearchScreenManager {
+    /* Building the control objects */
+    @FXML
+    Pane colaSearchPanel;
+    @FXML
+    TextField entryField;
+    @FXML
+    Ellipse searchButton;
+    @FXML
+    Text searchLabel;
+    @FXML
+    Polygon backButton;
+    @FXML
+    CheckBox beer, wine;
 }
