b5aeadded5a3beb8913248ef9949b030d9947228
Victor <spartanvice09@gmail.com>
2017-04-02 14:12:27 -0400

    Preliminary agent inbox

--- src/screen/LoginScreenManager.java
+++ src/screen/LoginScreenManager.java
@@ -6,10 +6,14 @@ import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 
+import java.awt.*;
+
 public class LoginScreenManager extends Screen{
 
+    private EnumScreenType type;
+    //set up the fxml fields and stuff
     @FXML
-    private TextField unsernameField;
+    private TextField usernameField;
 
     @FXML
     private Ellipse loginButton;
@@ -20,20 +24,28 @@ public class LoginScreenManager extends Screen{
     @FXML
     private Text loginLabel;
 
-    public LoginScreenManager(EnumScreenType type) {
-        super(type);
+    @FXML
+    private Button signSup;
+
+    //constructor
+    public LoginScreenManager() {
+        super(EnumScreenType.LOG_IN);
     }
 
-
+    //fxml methods
     @FXML
     void userLogin() {
-
-
+        String username = usernameField.getText();
     }
-
 
     @FXML
     void goBack() {
+        //tell the screen manager to set the screen to COLA screen
+        //screenManager.setScreen(EnumScreenType.LOG_IN);
+    }
 
+    @FXML
+    void userSignUp(){
+        //tell the screen manager to go to the create account screen
     }
 }
