b7b20254d9145524303b5dfe3f4418a222cea884
tj <tu.jo.robbins@gmail.com>
2017-04-03 20:48:32 -0400

    Added NewApplicationManager class

---
+++ src/screen/NewApplicationManager.java
@@ -0,0 +1,16 @@
+package screen;
+
+import base.LogManager;
+/**
+ * Created by tj on 4/3/2017.
+ */
+public class NewApplicationManager extends Screen {
+
+    public NewApplicationManager() {
+        super(EnumScreenType.MANUFACTURER_ADD_FORM);
+    }
+    /*fxml objects*/
+
+    /*fxml methods*/
+
+}
