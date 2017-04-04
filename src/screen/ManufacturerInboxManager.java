d12588e5fe290f3dbd6082383a8a82eab72a374e
mattfortmeyer <mattfortmeyer@gmail.com>
2017-04-04 18:03:10 -0400

    Merge branch 'master' of https://github.com/bhsostek/csgetdegrees
    # Conflicts:
    #	.idea/workspace.xml

--- src/screen/ManufacturerInboxManager.java
+++ src/screen/ManufacturerInboxManager.java
@@ -3,13 +3,14 @@ package screen;
 import base.LogManager;
 import javafx.scene.control.Button;
 import javafx.fxml.FXML;
+import javafx.scene.shape.Polygon;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 /**
  * Created by ${mrfortmeyer} on 4/3/2017.
 **/
-public class ManufacturerInboxManager {
+public class ManufacturerInboxManager extends Screen{
 
     @FXML
     private Button NewApplication;
@@ -18,7 +19,10 @@ public class ManufacturerInboxManager {
     private Button EditButton;
 
     @FXML
-    private Button BackButton;
+    private Polygon BackButton;
+
+    //constructer for the screen
+    public ManufacturerInboxManager() { super(EnumScreenType.MANUFACTURER_SCREEN);}
 
     public void goBack() {
         LogManager.println("Back button pressed from ManufacturerInboxScreen");
