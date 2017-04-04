0e2aa0926ee620f22f5810d16bfa87c08da7688f
vepina <spartanvice09@gmail.com>
2017-04-04 19:34:25 -0400

    update login and add account screens

--- src/screen/CreateAccountManager.java
+++ src/screen/CreateAccountManager.java
@@ -4,6 +4,7 @@ package screen;
  * Created by ${Victor} on 4/2/2017.
  */
 
+import base.LogManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -22,17 +23,39 @@ public class CreateAccountManager extends Screen{
     @FXML
     private Polygon backButton;
 
+
     /* Class methods */
     @FXML
     private void goBack(){
         //go back to the login screen
+        username.clear();
         ScreenManager.setScreen(EnumScreenType.LOG_IN);
         return;
     }
 
     @FXML
     private void makeAccount(){
-
+        String user = username.getText();
+        username.clear();
+        //query database to see if username is taken
+        if(user != "") { //placeholder for now
+            //tell the system who made an account
+            LogManager.println(user + " just made an account");
+            //record the account in the database
+            //TODO add user to database
+            //assume everyone is an agent for iteration 1
+            //go to the agent inbox
+            ScreenManager.setScreen(EnumScreenType.AGENT_INBOX);
+        }else {
+            //if name is taken, return to the make account screen
+            ScreenManager.setScreen(EnumScreenType.CREATE_ACCOUNT);
+        }
+        return;
+    }
+    @FXML
+    private void enterHit(){
+        makeAccount();
+        return;
     }
 }
 
