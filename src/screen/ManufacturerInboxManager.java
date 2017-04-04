582f42138dbb72cbbdcfd5791ad398ddee9f3f16
mattfortmeyer <mattfortmeyer@gmail.com>
2017-04-03 20:46:41 -0400

    Merge branch 'master' of https://github.com/bhsostek/csgetdegrees
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/screen/ManufacturerInboxManager.java
@@ -0,0 +1,22 @@
+package screen;
+
+import javafx.scene.control.Button;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * Created by ${mrfortmeyer} on 4/3/2017.
+**/
+public class ManufacturerInboxManager {
+
+    @FXML
+    private Button NewApplication;
+
+    @FXML
+    private Button EditButton;
+
+    @FXML
+    private Button BackButton;
+
+}
