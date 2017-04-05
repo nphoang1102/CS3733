bfe60e74574560c145323a6deb2ff9f84ae53d5f
Hoang Nguyen <nphoang1102@gmail.com>
2017-04-04 23:34:49 -0400

    implemented the tableView shit that I have just well spent 2 hours of my life on

--- src/screen/ColaSearchResultManager.java
+++ src/screen/ColaSearchResultManager.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import misc.ColaResult;
 
 /**
@@ -38,6 +39,10 @@ public class ColaSearchResultManager extends Screen{
         ObservableList<String> typeList = FXCollections.observableArrayList("Beer", "Wine", "Both");
         type.setItems(typeList);
         type.setValue("Beer");
+        this.id.setCellValueFactory(new PropertyValueFactory("id"));
+        this.source.setCellValueFactory(new PropertyValueFactory("source"));
+        this.alcoholType.setCellValueFactory(new PropertyValueFactory("type"));
+        this.name.setCellValueFactory(new PropertyValueFactory("name"));
     }
 
     public void onEnter() {
@@ -50,10 +55,20 @@ public class ColaSearchResultManager extends Screen{
         String toPrint = "User searches for " + this.keywords + " under type " + this.searchType;
         LogManager.println(toPrint);
         this.entryField.clear();
+        this.displayToTable();
     }
 
     public void backPressed() {
         LogManager.println("Back button pressed from ColaSearchResultScreen");
         ScreenManager.setScreen((EnumScreenType.LOG_IN));
+    }
+
+    public void displayToTable() {
+        ObservableList<ColaResult> resultTable = FXCollections.observableArrayList(
+                new ColaResult("", "", this.searchType+"", this.keywords+"")
+        );
+        this.searchResult.setEditable(false);
+
+        this.searchResult.setItems(resultTable);
     }
 }
