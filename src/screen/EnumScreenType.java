a500fd0283c43a643a3b9e58bfd344b28e755882
mattfortmeyer <mattfortmeyer@gmail.com>
2017-04-04 22:57:43 -0400

    Code stuff?

--- src/screen/EnumScreenType.java
+++ src/screen/EnumScreenType.java
@@ -13,25 +13,24 @@ import java.io.IOException;
  * Created by Bailey Sostek on 4/1/17.
  */
 public enum EnumScreenType {
-    LOG_IN("LoginScreen.fxml", new LoginScreenManager()),
-    CREATE_ACCOUNT("CreateAccount.fxml", new CreateAccountManager()),
-    COLA_SEARCH("ColaSearchScreen.fxml", new ColaSearchScreenManager()),
-//    COLA_SEARCH_RESULT(""),
-    MANUFACTURER_SCREEN("ManufacturerInbox.fxml", new ManufacturerInboxManager()),
-//    MANUFACTURER_VIEW_FORMS(""),
-//    MANUFACTURER_ADD_FORM(""),
-//    MANUFACTURER_EDIT(""),
-    AGENT_INBOX("AgentInbox.fxml", new AgentInboxManager()),
-    AGENT_APP_SCREEN("AgentAppScreen.fxml", new AgentAppScreenManager()),
+    LOG_IN("LoginScreen.fxml"),
+    CREATE_ACCOUNT("CreateAccount.fxml"),
+    COLA_SEARCH("ColaSearchScreen.fxml"),
+    COLA_SEARCH_RESULT(""),
+    MANUFACTURER_SCREEN("ManufacturerInbox.fxml"),
+    MANUFACTURER_VIEW_FORMS(""),
+    MANUFACTURER_ADD_FORM(""),
+    MANUFACTURER_EDIT(""),
+    AGENT_INBOX("AgentInbox.fxml"),
+    AGENT_APP_SCREEN("AgentAppScreen.fxml"),
     ;
 
     private Parent scene;
-    private Screen screen;
-
-    EnumScreenType (String file, Screen screen){
+    EnumScreenType (String file){
         try {
             this.scene = FXMLLoader.load(this.getClass().getResource("/screen/fxml/"+file));
-            this.screen = screen;
+        }catch (IOException e) {
+            LogManager.println("FXML file not found for:"+this.toString()+":"+ Main.PATH+Main.LOG+"/"+this.getFXMLFile(), EnumWarningType.WARNING);
         }catch(Exception e){
             LogManager.println(e.toString(), EnumWarningType.ERROR);
             LogManager.printStackTrace(e.getStackTrace());
@@ -40,8 +39,5 @@ public enum EnumScreenType {
 
     public Parent getFXMLFile(){
         return this.scene;
-    }
-    public Screen getScreen(){
-        return this.screen;
     }
 }
