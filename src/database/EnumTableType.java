76332b0a0ebda6889943fb4adffd86b33706c9f3
Evan Goldstein <ebgoldstein@wpi.edu>
2017-04-04 20:11:32 -0400

    Manufacturer Query methods.

--- src/database/EnumTableType.java
+++ src/database/EnumTableType.java
@@ -5,17 +5,7 @@ package database;
  */
 
 public enum EnumTableType {
-    ALCOHOL         ("ALCOHOL"),
-    MANUFACTURER    ("MANUFACTURER"),
-    APPLICATIONS    ("APPLICATIONS");
-
-    protected String table;
-
-    EnumTableType(String table){
-        this.table = table;
-    }
-
-    public String getPrefix(){
-        return this.table;
-    }
+    ALCOHOL,
+    MANUFACTURER,
+    APPLICATIONS
 }
