1f614ea34352311bb3353fceee749d09017ff9b3
Hoang Nguyen <nphoang1102@gmail.com>
2017-04-07 15:44:06 -0400

    added the mouse click event to the result table row

--- src/screen/ColaSearchResultManager.java
+++ src/screen/ColaSearchResultManager.java
@@ -38,31 +38,53 @@ public class ColaSearchResultManager extends Screen{
     @FXML
     TableView<ColaResult> searchResult;
     @FXML
-    TableColumn<ColaResult, String> id, source, alcoholType, name;
+    TableColumn<ColaResult, String> coLid, coLsource, coLalcoholType, coLname;
 
     /* Class methods */
     @FXML
     public void initialize() {
+        /* Initialize the choice box */
         ObservableList<String> typeList = FXCollections.observableArrayList("Beer", "Wine", "Other");
         type.setItems(typeList);
         type.setValue("Beer");
-        this.id.setCellValueFactory(new PropertyValueFactory("id"));
-        this.source.setCellValueFactory(new PropertyValueFactory("source"));
-        this.alcoholType.setCellValueFactory(new PropertyValueFactory("type"));
-        this.name.setCellValueFactory(new PropertyValueFactory("name"));
+
+        /* Setup properties for the columns in tableview */
+        this.coLid.setCellValueFactory(new PropertyValueFactory("id"));
+        this.coLsource.setCellValueFactory(new PropertyValueFactory("source"));
+        this.coLalcoholType.setCellValueFactory(new PropertyValueFactory("type"));
+        this.coLname.setCellValueFactory(new PropertyValueFactory("name"));
+
+        /* Allow the application to receive the row information on click, important shit right here */
+        searchResult.setRowFactory( tv -> {
+            TableRow<ColaResult> row = new TableRow();
+            row.setOnMouseClicked(event -> {
+                if ((event.getClickCount() == 1) && (! row.isEmpty())) {
+                    ColaResult rowData = row.getItem();
+                    LogManager.println(rowData.getName());
+                }
+            });
+            return row;
+        });
     }
 
+    /* When the enter button is hit, it is the same as a mouse click on the search button */
     public void onEnter() {
         this.buttonPressed();
     }
 
+    /* What to do when the search button is pressed */
     public void buttonPressed() {
+        /* Getting the keywords entered from user */
         resultTable = FXCollections.observableArrayList();
         this.keywords = entryField.getText();
         this.searchType = type.getValue() + "";
+
+        /* Print into our nifty log manager for debug purpose and clear the entry field */
         String toPrint = "User searches for " + this.keywords + " under type " + this.searchType;
         LogManager.println(toPrint);
         this.entryField.clear();
+
+        /* Send and receive result from the database and display them into the TableView */
         this.databaseResult = DatabaseManager.Search(this.keywords, this.searchType);
         for (DataSet tempSet: databaseResult) {
             String tempID = tempSet.getValueForKey("TTBID");
@@ -75,6 +97,7 @@ public class ColaSearchResultManager extends Screen{
         this.searchResult.setItems(resultTable);
     }
 
+    /* Hit back will bring you to the login screen */
     public void backPressed() {
         LogManager.println("Back button pressed from ColaSearchResultScreen");
         ScreenManager.setScreen((EnumScreenType.LOG_IN));
