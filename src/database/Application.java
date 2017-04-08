b2ec2e89017497e6602843b9d50ff48291682426
Evan Goldstein <ebgoldstein@wpi.edu>
2017-04-08 13:29:01 -0400

    Application Class

---
+++ src/database/Application.java
@@ -0,0 +1,32 @@
+package database;
+
+/**
+ * Created by Evan Goldstein on 4/6/17.
+ */
+public class Application extends DataSet{
+    String ApplicationNo;
+    String FancifulName;
+    String Manufacturer;
+    String BrandName;
+    String PermitNo;
+    String Status;
+    String AlcoholType;
+    String AgentID;
+    String Source;
+    String Brand;
+    String Address;
+    String Address2;
+    String Volume;
+    String ABV;
+    String PhoneNo;
+    String AppType;
+    String VintageDate;
+    String PH;
+    String InboxAgent;
+    String ApplicantName;
+    String DateSubmitted;
+    String DateApproved;
+    String DBAorTrade;
+    String Email;
+
+}
