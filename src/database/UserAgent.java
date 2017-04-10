3cae9b4e91b1ce9fd43a4518a404397aac24a732
Evan Goldstein <ebgoldstein@wpi.edu>
2017-04-10 13:03:08 -0400

    Renamed User child classes

---
+++ src/database/UserAgent.java
@@ -0,0 +1,16 @@
+package database;
+
+import screen.EnumUserType;
+
+/**
+ * Created by Evan Goldstein on 4/8/17.
+ */
+public class UserAgent extends User{
+    String ID;
+    public UserAgent(String name, String username, String email){
+        this.userType = EnumUserType.AGENT;
+        this.username = username;
+        this.email = email;
+        this.name = name;
+    }
+}
---
+++ src/database/UserManufacturer.java
@@ -0,0 +1,19 @@
+package database;
+
+/**
+ * Created by Evan Goldstein on 4/8/17.
+ */
+public class UserManufacturer extends DataSet {
+    String Address;
+    String Address2;
+    String Username;
+    String Password;
+    String Company;
+    String Name;
+    String RepID;
+    String PlantRegistry;
+    String Source;
+    String SerialNo;
+    String AlcoholType;
+    String PhoneNo;
+}
