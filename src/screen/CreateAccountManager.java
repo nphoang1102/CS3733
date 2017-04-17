a1ad0229a6e36dbaa37eeb82078d078cb4e52196
Victor <spartanvice09@gmail.com>
2017-04-17 10:55:22 -0400

    minor create account updates

--- src/screen/CreateAccountManager.java
+++ src/screen/CreateAccountManager.java
@@ -7,10 +7,7 @@ package screen;
 import base.*;
 import database.*;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 import sun.management.Agent;
@@ -39,6 +36,8 @@ public class CreateAccountManager extends Screen{
     //private CheckBox tickPublicUser;
     @FXML
     private Text accountError;
+    @FXML
+    private ProgressBar progressBar;
 
     EnumUserType userType;
 
